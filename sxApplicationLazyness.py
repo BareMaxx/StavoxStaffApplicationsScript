23bb06f89dbe6c6e5b78f670b7dd321598d89ea6
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2023-01-09 16:02:58 +0100

    Removed unused imports

--- sxApplicationLazyness.py
+++ sxApplicationLazyness.py
@@ -1,10 +1,7 @@
-from numpy import array
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from os.path import exists
